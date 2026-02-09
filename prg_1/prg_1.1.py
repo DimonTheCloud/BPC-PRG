a825dfbb3900296f583345ae533f89dc1317cfd1
DimonTheCloud <150388570+DimonTheCloud@users.noreply.github.com>
2026-02-09 13:17:24 +0100

    cv1

--- prg_1/prg_1.1.py
+++ prg_1/prg_1.1.py
@@ -11,3 +11,18 @@
 # print(name)
 # print(f"Vek:{age}")
 
+# course =  "Algoritmizace"
+# print(f"Vitejte v kurzu {course}")
+
+# monthly_income = 25000
+# MONTHS = 12
+# TAX_RATE_PCT = 0.15
+#
+# year_income = monthly_income * MONTHS
+# year_tax = year_income * TAX_RATE_PCT
+#
+# print(f"Year income: {year_income}")
+# print(f"Year tax: {year_tax}")
+
+
+
